4c671dc4b2583456a9355f39e430d28793b283e3
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-05-18 15:38:38 -0400

    import imgs

---
+++ src/utils/import_imgs/import-earrings-imgs.tsx
@@ -0,0 +1,41 @@
+import bohoHighImg from 'src/assets/imgs/earrings-imgs/high-res/boho-earrings-high.jpg'
+import bohoLowImg from 'src/assets/imgs/earrings-imgs/low-res/boho-earrings-low.jpg'
+import elephantHighImg from 'src/assets/imgs/earrings-imgs/high-res/elephant-earrings-high.jpg'
+import elephantLowImg from 'src/assets/imgs/earrings-imgs/low-res/elephant-earrings-low.jpg'
+import galaxyHighImg from 'src/assets/imgs/earrings-imgs/high-res/galaxy-earrings-high.jpg'
+import galaxyLowImg from 'src/assets/imgs/earrings-imgs/low-res/galaxy-earrings-low.jpg'
+import angelHighImg from 'src/assets/imgs/earrings-imgs/high-res/guardian-angel-earrings-high.jpg'
+import angelLowImg from 'src/assets/imgs/earrings-imgs/low-res/guardian-angel-earrings-low.jpg'
+import smallHoopsHighImg from 'src/assets/imgs/earrings-imgs/high-res/jasmin-chew-UBeNYvk6ED0-high.jpg'
+import smallHoopsLowImg from 'src/assets/imgs/earrings-imgs/low-res/jasmin-chew-UBeNYvk6ED0-low.jpg'
+import tearDropHighImg from 'src/assets/imgs/earrings-imgs/high-res/julia-cheperis-GFI1sRmDaCs-high.jpg'
+import tearDropLowImg from 'src/assets/imgs/earrings-imgs/low-res/julia-cheperis-GFI1sRmDaCs-low.jpg'
+import pearlHighImg from 'src/assets/imgs/earrings-imgs/high-res/kateryna-hliznitsova-P6NiFTyI294-high.jpg'
+import pearlLowImg from 'src/assets/imgs/earrings-imgs/low-res/kateryna-hliznitsova-P6NiFTyI294-low.jpg'
+import marbleStudHighImg from 'src/assets/imgs/earrings-imgs/high-res/marble-stud-earrings-high.jpg'
+import marbleStudLowImg from 'src/assets/imgs/earrings-imgs/low-res/marble-stud-earrings-low.jpg'
+import goldHighImg from 'src/assets/imgs/earrings-imgs/high-res/sama-hosseini-6seIjO8gX2w-high.jpg'
+import goldLowImg from 'src/assets/imgs/earrings-imgs/low-res/sama-hosseini-6seIjO8gX2w-low.jpg'
+
+const earringImgs = {
+    bohoHighImg,
+    bohoLowImg,
+    elephantHighImg,
+    elephantLowImg,
+    galaxyHighImg,
+    galaxyLowImg,
+    angelHighImg,
+    angelLowImg,
+    smallHoopsHighImg,
+    smallHoopsLowImg,
+    tearDropHighImg,
+    tearDropLowImg,
+    pearlHighImg,
+    pearlLowImg,
+    marbleStudHighImg,
+    marbleStudLowImg,
+    goldHighImg,
+    goldLowImg
+}
+
+export default earringImgs
