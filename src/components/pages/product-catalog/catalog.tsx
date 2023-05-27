27e345a252a558651453ecd41fa18941a725f339
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-05-27 12:02:32 -0400

    add few tweaks

--- src/components/pages/product-catalog/catalog.tsx
+++ src/components/pages/product-catalog/catalog.tsx
@@ -5,19 +5,23 @@ import '../../../style/css/catalog.css'
 
 interface Data {
     data: typeof braceletData,
+    title: string
     addFavorite: (id:string) => void,
     addToCart: (id:string) => void
 }
 
 
-const Catalog = ({data, addToCart,addFavorite}:Data) => {
+const Catalog = ({data,title,addToCart,addFavorite}:Data) => {
     return (
-        <div className="catalog">
-            {data.map((d) => (
-            <Link className="product-link" to={d.id} key={d.id}>
-                <Card cardInfo={d} addFavorite={() => addFavorite(d.id)} addToCart={() => addToCart(d.id)} ></Card>
-            </Link>
-        ))}
+        <div className="container">
+            <h1>{title}</h1>
+            <div className="catalog">
+                {data.map((d) => (
+                <Link className="product-link" to={d.id} key={d.id}>
+                    <Card cardInfo={d} addFavorite={() => addFavorite(d.id)} addToCart={() => addToCart(d.id)} ></Card>
+                </Link>
+            ))}
+            </div>
         </div>
     )
 }
