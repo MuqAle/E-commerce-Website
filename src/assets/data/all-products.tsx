d41338900029953aeaa5d509eeac4660c7c3312a
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-05-27 12:05:12 -0400

    create db with all products

---
+++ src/assets/data/all-products.tsx
@@ -0,0 +1,7 @@
+import braceletData from "./bracelet-data";
+import earringData from "./earrings-data";
+import necklaceData from "./necklace-data";
+
+const allData = [...braceletData,...earringData,...necklaceData]
+
+export default allData
