6f802582dd27b318a454c361eb8651a12947bb60
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-05-30 16:08:35 -0400

    change formatting

--- src/components/pages/product-catalog/catalog.tsx
+++ src/components/pages/product-catalog/catalog.tsx
@@ -2,6 +2,8 @@ import Card from "./card";
 import braceletData from "../../../assets/data/bracelet-data";
 import { Link } from "react-router-dom";
 import '../../../style/css/catalog.css'
+import heartCircle from '../../../assets/imgs/svg-imgs/heart-circle.svg'
+import shoppingBag from '../../../assets/imgs/svg-imgs/shopping-bag-circle.svg'
 
 interface Data {
     data: typeof braceletData,
@@ -14,12 +16,16 @@ interface Data {
 const Catalog = ({data,title,addToCart,addFavorite}:Data) => {
     return (
         <div className="container">
-            <h1>{title}</h1>
+            <h1 className="catalog-title">{title}</h1>
             <div className="catalog">
                 {data.map((d) => (
-                <Link className="product-link" to={d.id} key={d.id}>
-                    <Card cardInfo={d} addFavorite={() => addFavorite(d.id)} addToCart={() => addToCart(d.id)} ></Card>
-                </Link>
+                <div className="card-container" key={d.id}>
+                    <Link className="product-link" to={d.id} >
+                        <Card cardInfo={d}></Card>
+                    </Link>
+                <button className="left-btn" onClick={()=> addFavorite(d.id)}><img src={heartCircle} alt="add-favorite" /></button>
+                <button className="right-btn" onClick={() => addToCart(d.id)}><img src={shoppingBag} alt='add-to-cart'></img></button>
+                </div>
             ))}
             </div>
         </div>
