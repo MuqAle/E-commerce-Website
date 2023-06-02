b070996045df5ac6f51df3d815682f134ac9bc97
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-06-02 11:11:22 -0400

    change imgs

--- src/components/pages/product-catalog/catalog.tsx
+++ src/components/pages/product-catalog/catalog.tsx
@@ -1,29 +1,31 @@
 import Card from "./card";
 import braceletData from "../../../assets/data/bracelet-data";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import '../../../style/css/catalog.css'
-import heartCircle from '../../../assets/imgs/svg-imgs/heart-circle.svg'
-import shoppingBag from '../../../assets/imgs/svg-imgs/shopping-bag-circle.svg'
+import heartOutline from '../../../assets/imgs/svg-imgs/heart-outline.svg'
+import heartFilled from '../../../assets/imgs/svg-imgs/heart-filled.svg'
+import shoppingBag from '../../../assets/imgs/svg-imgs/shopping-bag-white.svg'
 
 interface Data {
     data: typeof braceletData,
     title: string
     addFavorite: (id:string) => void,
     addToCart: (id:string) => void
+    favorited: (id:string) => boolean
 }
 
 
-const Catalog = ({data,title,addToCart,addFavorite}:Data) => {
+const Catalog = ({data,title,addToCart,addFavorite, favorited}:Data) => {
     return (
         <div className="container">
             <h1 className="catalog-title">{title}</h1>
             <div className="catalog">
                 {data.map((d) => (
                 <div className="card-container" key={d.id}>
-                    <Link className="product-link" to={d.id} >
+                    <NavLink className="product-link" to={d.id} >
                         <Card cardInfo={d}></Card>
-                    </Link>
-                <button className="left-btn" onClick={()=> addFavorite(d.id)}><img src={heartCircle} alt="add-favorite" /></button>
+                    </NavLink>
+                <button className="left-btn" onClick={()=> addFavorite(d.id)}><img src={favorited(d.id) ? heartFilled:heartOutline} alt="add-favorite" /></button>
                 <button className="right-btn" onClick={() => addToCart(d.id)}><img src={shoppingBag} alt='add-to-cart'></img></button>
                 </div>
             ))}
