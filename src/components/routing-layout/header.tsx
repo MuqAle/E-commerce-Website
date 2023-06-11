7c98d72c35a144bd4cd8f55ba2821c90f0ef872a
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-06-11 18:33:17 -0400

    move footer and header to own components

---
+++ src/components/routing-layout/header.tsx
@@ -0,0 +1,92 @@
+import { NavLink } from 'react-router-dom'
+import { useState,useEffect} from "react";
+import heartOutline from '../../assets/imgs/svg-imgs/heart-outline.svg'
+import shoppingBag from '../../assets/imgs/svg-imgs/shopping-bag.svg'
+import search from '../../assets/imgs/svg-imgs/search.svg'
+import logo from '../../assets/imgs/svg-imgs/icons8-logo.svg'
+import menu from '../../assets/imgs/svg-imgs/menu_FILL0_wght100_GRAD0_opsz48.svg'
+import close from '../../assets/imgs/svg-imgs/close_FILL0_wght100_GRAD0_opsz48.svg'
+import arrow from '../../assets/imgs/svg-imgs/arrow.svg'
+
+interface Header{
+    shoppingCart:number,
+    favorites:number
+}
+
+const Header = ({favorites, shoppingCart}:Header) => {
+
+    const [openMenu,setOpenMenu] = useState(false)
+    const [openSearch, setOpenSearch] = useState(false)
+
+    const toggleMenu = () => {
+        setOpenMenu((menu) => !menu )
+    }
+
+    const toggleSearch = () => {
+        setOpenSearch(search => !search)
+    }
+
+    useEffect(() => {
+        const body = document.querySelector('body');
+        if(body){
+            body.style.overflow = openMenu ? 'hidden' : 'auto';
+        }
+      }, [openMenu])
+
+    useEffect(() => {
+        const search = document.querySelector<HTMLInputElement>('.input-search')
+        if(search){
+            search.focus()
+        }
+    },[openSearch])
+      return(
+        <header>
+        <nav>
+            <NavLink id="logo" to={"/"}>
+                    <img src={logo} alt="logo" />
+            </NavLink>
+            <div id="left-side-nav">
+                <NavLink className = 'header-link' to={"shop-all"}>Shop All</NavLink>
+                <NavLink className = 'header-link' to={"necklace"}>Necklaces</NavLink>
+                <NavLink className = 'header-link' to={"bracelet"}>Bracelets</NavLink>
+                <NavLink className = 'header-link' to={"earrings"}>Earrings</NavLink>
+                <NavLink className = 'header-link' to={"on-sale"}>On Sale</NavLink>
+                <NavLink className = 'header-link' to={"about-us"}>About Us</NavLink>
+            </div>
+            <div className="nav-bar-container" style={openMenu ? {background:'rgba(0, 0, 0, 0.6)' } : {background:'transparent',visibility:'hidden'}}>
+                <div id="left-side-nav-mobile" style={openMenu ? {width:'100%'} : {width:'0px'}}>
+                     <button className="close-menu" onClick={toggleMenu}><img src={close} alt="close menu" /></button>
+                    <NavLink className = 'header-link' to={"shop-all"} onClick={toggleMenu}>Shop All</NavLink>
+                    <NavLink className = 'header-link' to={"necklace"} onClick={toggleMenu}>Necklaces</NavLink>
+                    <NavLink className = 'header-link' to={"bracelet"} onClick={toggleMenu}>Bracelets</NavLink>
+                    <NavLink className = 'header-link' to={"earrings"} onClick={toggleMenu}>Earrings</NavLink>
+                    <NavLink className = 'header-link' to={"on-sale"} onClick={toggleMenu}>On Sale</NavLink>
+                    <NavLink className = 'header-link' to={"about-us"} onClick={toggleMenu}>About Us</NavLink>
+                </div>
+            </div>
+            <div id="right-side-nav">
+                <button onClick={toggleSearch} className="btn-search">
+                    <img src={search} alt="search" />
+                </button>
+                <NavLink id="wish-list" to={"wish-list"}>
+                    {favorites === 0? null:<span className="favorite-span">{favorites}</span>}
+                    <img src={heartOutline} alt="favorite-item"/>
+                </NavLink>
+                <NavLink id="shopping-cart" to={"shopping-cart"}>
+                    {shoppingCart === 0?null:<span className="shopping-cart-span">{shoppingCart}</span>}
+                    <img src={shoppingBag} alt="add-to-cart"/>
+                </NavLink>
+                <button className="menu" onClick={toggleMenu}><img src={menu} alt="open menu" /></button>
+            </div>
+        </nav>
+        <div className="input-container" style={openSearch ? {height:'80px'} : {height:'0px',padding:0}}>
+            <div className="search-container">
+                <input className="input-search" type="text"></input>
+                <button className="search-submit"><img src={arrow}/></button>
+            </div>
+        </div>
+    </header>
+    )
+}
+
+export default Header
--- src/components/routing-layout/root-layout.tsx
+++ src/components/routing-layout/root-layout.tsx
@@ -1,101 +1,27 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "./header";
+import Footer from "./footer";
 import ScrollToTop from "../../utils/scroll-to-top";
-import heartOutline from '../../assets/imgs/svg-imgs/heart-outline.svg'
-import shoppingBag from '../../assets/imgs/svg-imgs/shopping-bag.svg'
-import search from '../../assets/imgs/svg-imgs/search.svg'
-import logo from '../../assets/imgs/svg-imgs/icons8-logo.svg'
-import arrow from '../../assets/imgs/svg-imgs/arrow.svg'
-import '../../style/css/root.css'
 
+
+import '../../style/css/root.css'
 
 interface Span{
     shoppingCart:number,
     favorites:number
 }
+
 const RootLayout = ({shoppingCart,favorites}:Span) => {
 
     return(
         <div id="main-container">
             <ScrollToTop/>
-            <header>
-                <nav>
-                    <NavLink id="logo" to={"/"}>
-                            <img src={logo} alt="logo" />
-                    </NavLink>
-                    <div id="left-side-nav">
-                        <NavLink className = 'header-link' to={"shop-all"}>Shop All</NavLink>
-                        <NavLink className = 'header-link' to={"necklaces"}>Necklaces</NavLink>
-                        <NavLink className = 'header-link' to={"bracelets"}>Bracelets</NavLink>
-                        <NavLink className = 'header-link' to={"earrings"}>Earrings</NavLink>
-                        <NavLink className = 'header-link' to={"on-sale"}>On Sale</NavLink>
-                        <NavLink className = 'header-link' to={"about"}>About Us</NavLink>
-                    </div>
-                    <div id="right-side-nav">
-                        <div className= 'search-box'>
-                            <button className="btn-search">
-                                <img src={search} alt="search" />
-                            </button>
-                            <input className="input-search" type="text"></input>
-                        </div>
-                        
-                        <NavLink id="wish-list" to={"wish-list"}>
-                            {favorites === 0? null:<span className="favorite-span">{favorites}</span>}
-                            <img src={heartOutline} alt="favorite-item"/>
-                        </NavLink>
-                        <NavLink id="shopping-cart" to={"shopping-cart"}>
-                            {shoppingCart === 0?null:<span className="shopping-cart-span">{shoppingCart}</span>}
-                            <img src={shoppingBag} alt="add-to-cart"/>
-                        </NavLink>
-                    </div>
-                </nav>
-            </header>
+            <div className="sign-up-header">Get 10% Off Your First Order When You <button>Sign Up!</button></div>
+            <Header favorites={favorites} shoppingCart={shoppingCart}/>
             <main>
                 <Outlet/>
             </main>
-            <footer>
-                <div className="discount">
-                    <label htmlFor="email-signup">SIGN UP FOR 10% OFF</label>
-                    <div className="input-box">
-                        <input id="email-signup" placeholder="Enter your email address"></input>
-                        <button type="submit"><img src={arrow} alt="arrow"/> </button>
-                    </div>
-                    <div>
-                        <input type="checkbox" id='sign-up-checkbox'></input>
-                        <label htmlFor="sign-up-checkbox">By signing up you agree to our <a>Privacy Policy</a></label>
-                    </div>
-                </div>
-                <div id="info">
-                    <div className="info-links">
-                        <h3>Help</h3>
-                            <ul>
-                                <li><a>FAQs</a></li>
-                                <li><a>Shipping</a></li>
-                                <li><a>Returns</a></li>
-                                <li><a>Materials and Care</a></li>
-                                <li><a>Contact Us</a></li>
-                            </ul>
-                    </div>
-                    <div className="info-links">
-                        <h3>About Us</h3>
-                            <ul>
-                                <li><a>Rewards</a></li>
-                                <li><a>Careers</a></li>
-                                <li><a>Sustainability</a></li>
-                                <li><a>Blog</a></li>
-                            </ul>
-                    </div>
-                    <div className="info-links">
-                        <h3>More Info</h3>
-                            <ul>
-                                <li><a>Terms and Conditions</a></li>
-                                <li><a>Privacy Policy</a></li>
-                                <li><a>Student Discount</a></li>
-                                <li><a>Refer a Friend</a></li>
-                                <li><a>Security</a></li>
-                            </ul>
-                    </div>
-                </div>
-            </footer>
+            <Footer/>
         </div>
     )
 }
