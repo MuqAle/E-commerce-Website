a771fb069bab044c6a5e467ebd7c7283d193df18
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-08-30 15:10:52 -0400

    create cart schema

---
+++ backend/src/models/shopping-cart-model.ts
@@ -0,0 +1,41 @@
+import mongoose, {Schema,Types} from "mongoose";
+import { CartTypes } from "../types/type";
+
+
+const cartSchema = new mongoose.Schema({
+    products:[{
+        product:{
+            type:Schema.Types.ObjectId,
+            ref:'Product'
+        },
+        quantity:{
+            type:Number,
+            required:true
+        },
+        price:{
+            type:Number,
+            required:true
+        }
+    }],
+    cartTotal:Number,
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    
+},
+{
+    timestamps:true
+})
+
+const Cart = mongoose.model<CartTypes>('Cart', cartSchema)
+
+cartSchema.set('toJSON', {
+    transform: (_document, returnedObject) => {
+        returnedObject.id = (returnedObject._id as Types.ObjectId).toString()
+        delete returnedObject._id
+        delete returnedObject._v
+    }
+})
+
+export default Cart
