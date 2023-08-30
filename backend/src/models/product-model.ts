c558021b3aa6b6fdb43ecd4a64a2d0ed8e444cb4
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-08-30 13:48:39 -0400

    expand product model

--- backend/src/models/product-model.ts
+++ backend/src/models/product-model.ts
@@ -1,33 +1,58 @@
 import mongoose from "mongoose";
 import { Types } from "mongoose";
-
+import { ProductDb } from "../types/type";
 
 const inventorySchema = new mongoose.Schema({
     name:{
         type:String,
-        required:[true,'name is required']
+        required:[true,'Name is required']
     },
-    idProduct:String,
     type:String,
     price:{
         type:Number,
-        required:[true,'price is required']
+        required:[true,'Price is required']
     },
     onSale:{
         type:Boolean,
-        required:[true,'item should be specified if on sale']
+        required:[true,'Item should be specified if on sale']
     },
     salePercentage:Number,
     description:String,
+    stock:{
+        type:Number,
+        required:[true,'Must know how many in stock']
+    },
+    sold:{
+        type:Number,
+        default:0
+    },
+    metal:{
+        type:String,
+        enum:['gold','silver','brass']
+    },
+    colors:[String],
+    overallRating:{
+        type:Number,
+        default:0},
+    reviews:[{
+        postedBy:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'User',
+        },
+        reviewDesc: String,
+        rating: Number,
+        datePosted:{
+            type:Date,
+            default:Date.now()
+        }
+    }],
     images:[{
         type:String,
         required:[true,'an image must be uploaded']
     }]
 })
 
-const Product = mongoose.model('Product', inventorySchema)
-
-
+const Product = mongoose.model<ProductDb>('Product', inventorySchema)
 
 
 inventorySchema.set('toJSON',{
