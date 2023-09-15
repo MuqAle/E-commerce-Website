385cc958d19b6a2b9f9b2260ed31353db23ac261
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-09-15 01:18:18 -0400

    move express session to middleware file

---
+++ backend/src/middleware/express-session.ts
@@ -0,0 +1,20 @@
+import MongoStore from "connect-mongo";
+import { MONGODB_URI, SESSION_KEY } from "../config/config";
+import session from "express-session";
+
+
+const sessionStore = MongoStore.create({
+    mongoUrl:MONGODB_URI,
+    dbName:'guestUsers',
+    stringify:false,
+    ttl: 14 * 24 * 60 * 60
+})
+
+const sessionOptions:session.SessionOptions  = {
+    secret:SESSION_KEY as string,
+    resave:false,
+    saveUninitialized:false,
+    store: sessionStore
+}
+
+export {sessionOptions,sessionStore}
