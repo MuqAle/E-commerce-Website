eb60d0cfb1e66f52f3da89f0b579b7cc3cbb921f
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-09-19 13:03:38 -0400

    create delete review fnc

--- backend/src/controller/reviews-controller.ts
+++ backend/src/controller/reviews-controller.ts
@@ -1,13 +1,31 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Product from "../models/product-model";
 import { ParamsDictionary } from "express-serve-static-core"
 import { ReviewTypes } from "../types/type";
 import mongoose from "mongoose";
 import User from "../models/user-model";
+import evenRound from "../utils/rounding";
 
 
 
-const postReview = async (req:Request<ParamsDictionary, unknown, ReviewTypes>,res:Response) => {
+const updatedRating = async (id:string) => {
+
+    const getAllReviews = await Product.findById(id)
+        const totalReviews = getAllReviews?.reviews?.length
+        let actualRating
+        const reviewSum = getAllReviews?.reviews?.map((product) => product.rating)
+        .reduce((prev,curr) => prev + curr, 0)
+        if(reviewSum && totalReviews){
+             actualRating = evenRound((reviewSum/totalReviews),3)
+             getAllReviews.overallRating = actualRating
+        }
+        const product = await getAllReviews?.save()
+
+        return product
+}
+
+
+const postReview = async (req:Request<ParamsDictionary, unknown, ReviewTypes>,res:Response,next:NextFunction) => {
     try{
         const {user} = req
         const {rating,reviewDesc} = req.body 
@@ -52,7 +70,7 @@ const postReview = async (req:Request<ParamsDictionary, unknown, ReviewTypes>,re
                     reviews:{
                         postedBy:user.id,
                         rating:rating,
-                        reviewDesc:reviewDesc
+                        reviewDesc:reviewDesc, 
                     }
                 },
                 new:true,
@@ -61,28 +79,48 @@ const postReview = async (req:Request<ParamsDictionary, unknown, ReviewTypes>,re
             user.reviews = user.reviews?.concat({
                 product:new mongoose.Types.ObjectId(req.params.id),
                 reviewDesc:reviewDesc,
-                rating:rating})
+                rating:rating,})
             await user.save()
         }
-        const getAllReviews = await Product.findById(req.params.id)
-        const totalReviews = getAllReviews?.reviews?.length
-        let actualRating
-        const reviewSum = getAllReviews?.reviews?.map((product) => product.rating)
-        .reduce((prev,curr) => prev + curr, 0)
-        if(reviewSum && totalReviews){
-             actualRating = Math.round(reviewSum/totalReviews)
-        }
-        const finalProduct = await Product.findByIdAndUpdate(req.params.id,{
-            overallRating:actualRating
-        },{
-            new:true
-        })
+        const finalProduct = await updatedRating(req.params.id)
 
         res.json(finalProduct)
+
     }catch(error){
-        console.log(error)
-        res.status(400).send(error)
+        next(error)
     }
 }
 
-export default postReview
+const deleteReview = async (req:Request,res:Response,next:NextFunction) => {
+    try{
+        const {user} = req
+
+        const product = await Product.findById(req.params.id)
+
+        const reviewPosted = product?.reviews?.find(
+            userId => userId.postedBy.toString() === user.id.toString()
+        )
+        if(reviewPosted && (product && product.reviews) && user.reviews){
+            console.log(reviewPosted)
+            const productIndex = product.reviews.findIndex(review => review.postedBy === reviewPosted.postedBy)
+            console.log(productIndex)
+            product.reviews = product.reviews.filter(review => reviewPosted.postedBy !== review.postedBy)
+            console.log(product.reviews)
+            const userIndex = user.reviews.findIndex(review => review.product.toString() === req.params.id)
+            console.log(userIndex)
+            user.reviews = user.reviews.filter(review => review.product.toString() !== req.params.id)
+            console.log(user.reviews)
+            await user.save()
+            await product.save()
+            const finalProduct = await updatedRating(req.params.id)
+            console.log(finalProduct)
+        }
+        else{
+            res.status(400).send('no review or product found')
+        }
+    }catch(error){
+        next(error)
+    }
+}
+
+export {postReview,deleteReview}
