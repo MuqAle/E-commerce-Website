c7656fd3126661a95c3ee3b1d294a2fd0d8e9d80
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-09-19 19:14:01 -0400

    Move cart updating logic to separate folder

--- backend/src/controller/cart-controller.ts
+++ backend/src/controller/cart-controller.ts
@@ -1,26 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import mongoose from "mongoose"
-import User from "../models/user-model"
+import { Types } from "mongoose"
+import { addToCartGuest, 
+    addToCartUser,
+    deleteFromCartGuest,
+    deleteFromCartUser,
+    reduceFromCartGuest,
+    reduceFromCartUser} from "../services/cart-services"
 import Cart from "../models/shopping-cart-model"
 import { NextFunction, Request, Response } from "express"
 import {  CartTypes, ProductDb} from "../types/type"
 import Product from "../models/product-model"
-import evenRound from "../utils/rounding"
 
 
 
-interface CartType {
-    product:{
-        _id:mongoose.Types.ObjectId,
-        price:number
-        name:string
-        onSale:boolean,
-        salePercentage:number
-        salePrice:number
-    }
-    quantity:number,
-    _id:mongoose.Types.ObjectId
-}
+
+
 
 const getAllCart = async(_req:Request,res:Response,) => {
     const cart:CartTypes[] = await Cart.find({}).populate('products.product', {name:1,price:1})
@@ -28,91 +22,38 @@ const getAllCart = async(_req:Request,res:Response,) => {
      res.json(cart)
 }
 
-
 const addToCart = async (req:Request,res:Response,next:NextFunction) => {
     const productId = req.params.id
-    
-
     const cartItems = {
-        product:new mongoose.Types.ObjectId(productId),
+        product:new Types.ObjectId(productId),
         quantity:1
     }
-    const product = await Product.findById(productId) as ProductDb
     try{
-        if(req.user){
-            const userId = req.user.id 
-            const user = await User.findById(userId)
-            const cart = await Cart.findById(user?.shoppingCart).populate('products.product',{price:1,name:1,onSale:1,salePercentage:1,salePrice:1}) 
-            const foundObject = cart?.products.find(product => productId === product.product.id.toString()) as CartType | undefined
-            
-            if(!foundObject){
-               const updatedCart =  await Cart.findByIdAndUpdate(cart?._id,
-                    {
-                    $push:{products:cartItems},
-                    $inc:{cartTotal:cartItems.quantity, cartPrice: product.onSale ?
-                          product.salePrice
-                         :
-                         product.price },
-                    },
-                    { 
-                    new:true
-                    }).populate('products.product',{price:1,name:1,onSale:1,salePercentage:1})
+        const product = await Product.findById(productId) as ProductDb
+        const user = req.user
 
-                    res.json(updatedCart)
-            }else{
-                foundObject.quantity += 1
-               if(cart && foundObject){
-                cart.cartPrice += foundObject.product.onSale ? 
-                foundObject.product.salePrice
-                : 
-                foundObject.product.price
-                cart.cartPrice = evenRound(cart.cartPrice,2) 
-                cart.cartTotal += 1        
-                const savedCart = await cart.save()
-                res.json(savedCart)    
-               }
+        if(product){
+
+            if(user){
+                const updatedCart = addToCartUser(user,productId,cartItems,product)
+    
+                res.status(200).json(updatedCart)
             }
-            
-        }
-        else{
-            
-            let guestCart = req.session.guestCart as CartTypes
-            const foundObject = guestCart?.products?.find(product => productId === product.product.toString())
-
-            if(!guestCart){
-                guestCart = {
-                    products:[] as unknown as CartTypes['products'],
-                    cartPrice:0,
-                    cartTotal:0
-                } 
+            else{
+                const guestCart = req.session.guestCart as CartTypes
                 
-            }if(foundObject && guestCart){
-                foundObject.quantity += 1
-                guestCart.cartPrice += product.onSale ?
-                product.salePrice as number
-                :
-                product.price
-                guestCart.cartTotal += 1
-                guestCart.cartPrice = evenRound(guestCart.cartPrice,2)
-            }else{
-                guestCart.products.push(cartItems)
-                guestCart.cartPrice += product.onSale ?
-                product.salePrice as number
-                :
-                product.price
-                guestCart.cartTotal += 1
+                const newGuestCart = addToCartGuest(guestCart,cartItems,productId,product)
+    
+                req.session.guestCart = newGuestCart
+       
+                res.status(200).json(newGuestCart)
             }
-
-            guestCart.cartPrice = evenRound(guestCart.cartPrice,2)
-
-            req.session.guestCart = guestCart
-   
-            res.send(guestCart)
+        }else{
+            res.status(400).json({error:'This item does not exist'})
         }
-        
+
     }catch(error){
-        next(error)
-        console.log(error)
+        next()
     }
    
 }
@@ -120,61 +61,23 @@ const addToCart = async (req:Request,res:Response,next:NextFunction) => {
 const deleteFromCart = async (req:Request,res:Response,next:NextFunction) => {
 
     const productId = req.params.id
-
+    const user = req.user
 
     try{
-        if(req.user){
-            const userShopping = req.user.shoppingCart
-            const cart = await Cart.findById(userShopping).populate('products.product',{price:1,name:1,onSale:1,salePercentage:1,salePrice:1}) 
-            const foundObject = cart?.products.find(product => productId === product.product.id.toString()) as CartType | undefined
-            
-            if(!foundObject){
-                next()
+        if(user){
+            const updatedCart = await deleteFromCartUser(user,productId)
+            if(updatedCart){
+                res.status(updatedCart.status).json(updatedCart.response)
             }
-            if(cart && cart?.products.length < 1){
-                next()
-            }else{
-                
-                if(foundObject){
-                    const updatedCart = await Cart.findByIdAndUpdate(cart?._id,
-                        {
-                        $pull:{products:{product:productId}},
-                        $inc:{cartTotal: -foundObject?.quantity, cartPrice: evenRound(
-                            foundObject.product.onSale ?
-                             evenRound(-(foundObject.product.salePrice * foundObject.quantity),2)
-                            :
-                            evenRound(-(foundObject.product.price * foundObject.quantity),2),2
-                        ) 
-                             }
-                        },
-                        { 
-                        new:true
-                        })
-                        res.json(updatedCart)
-                }
-                
-            }
-            
         }else{
-            const anonCart =  req.session.guestCart as CartTypes
-            const foundIndex = anonCart.products.findIndex(product => productId === product.product.toString())
-            const foundProduct = anonCart.products.find(product => productId === product.product.toString())
-            const product = await Product.findById(foundProduct?.product)
-            if(foundProduct && product){
-                anonCart.products.splice(foundIndex,1)
-                anonCart.cartPrice -= product?.onSale ? 
-                evenRound((product.salePrice as number) * foundProduct.quantity,2) :
-                evenRound(product.price * foundProduct.quantity,2)
-                anonCart.cartPrice = evenRound(anonCart.cartPrice,2)
-                anonCart.cartTotal -= foundProduct.quantity
-            }
+            const guestCart =  req.session.guestCart as CartTypes
+            const updatedCart = await deleteFromCartGuest(guestCart,productId)
   
-            res.json(anonCart)
+            res.status(updatedCart.status).json(updatedCart.response)
         }
 
     }catch(error){
         next()
-        console.log(error)
     }
 }
 
@@ -187,54 +90,20 @@ const reductionCart = async (req:Request,res:Response,next:NextFunction) => {
 
     try{
         if(req.user){
-            const userId = req.user.shoppingCart
-            const cart = await Cart.findById(userId).populate('products.product',{price:1,name:1,onSale:1,salePercentage:1,salePrice:1}) 
-            const foundObject = cart?.products.find(product => productId === product.product.id.toString()) as CartType | undefined
-            
-            if(foundObject && foundObject.quantity > 1){
-                foundObject.quantity -= 1
-            }else{
-                await Cart.findByIdAndUpdate(cart?._id,
-                    {
-                    $pull:{products:{product:productId}},
-                    },
-                    { 
-                    new:true
-                    })
-
+            const updatedCart = await reduceFromCartUser(req.user,productId)
+            if(updatedCart){
+                res.status(updatedCart?.status).json(updatedCart?.response)
             }
-            if(cart && foundObject){
-
-                cart.cartTotal -= 1
-                cart.cartPrice -= foundObject.product.onSale ? 
-                foundObject.product.salePrice
-                : 
-                foundObject.product.price 
-                cart.cartPrice = evenRound(cart.cartPrice,2)
-                await cart.save()
-            }
-            if(cart && cart.products.length < 1){
-                next()
-            }
-            res.json(await Cart.findById(userId))
         }else{
-
-            const anonCart =  req.session.guestCart as CartTypes
-            const foundProduct = anonCart.products.find(product => productId === product.product.toString())
-            const product = await Product.findById(foundProduct?.product) as ProductDb
-            const foundIndex = anonCart.products.findIndex(product => productId === product.product.toString())
-            if(foundProduct && foundProduct.quantity > 1){
-                foundProduct.quantity -= 1
-            }else{
-                anonCart.products.splice(foundIndex,1)
+            const guestCart = req.session.guestCart
+            if(guestCart){
+                if(guestCart.products.length < 1){
+                    res.status(400).json({Error:'No Items In Cart'})
+                }else{
+                   const updatedGuestCart =  await reduceFromCartGuest(guestCart,productId)
+                   res.status(200).json(updatedGuestCart)
+                }
             }
-            anonCart.cartPrice -= product.onSale ? 
-            product.salePrice as number
-            :
-            product.price
-            anonCart.cartTotal -= 1
-            anonCart.cartPrice = evenRound(anonCart.cartPrice,2)
-            res.json(anonCart)
         }
     }catch(error){
         next()
---
+++ backend/src/services/cart-services.ts
@@ -0,0 +1,224 @@
+import { Types } from "mongoose"
+import Cart from "../models/shopping-cart-model"
+import { CartTypes, ProductDb, UserTypes } from "../types/type"
+import evenRound from "../utils/rounding"
+import Product from "../models/product-model"
+
+interface CartType {
+    product:{
+        _id:Types.ObjectId,
+        price:number
+        name:string
+        onSale:boolean,
+        salePercentage:number
+        salePrice:number
+    }
+    quantity:number,
+    _id:Types.ObjectId
+}
+
+
+const addToCartUser = async (user:UserTypes,productId:string,cartItems:CartTypes['products'][0],product:ProductDb) => {
+        const cart = await Cart.findById(user.shoppingCart).
+            populate('products.product',{price:1,name:1,onSale:1,salePercentage:1,salePrice:1}) 
+        const foundObject = cart?.products.
+            find(product => productId === product.product.id.toString()) as CartType | undefined
+        let updatedCart
+        if(!foundObject){
+                updatedCart =  await Cart.findByIdAndUpdate(cart?._id,
+                 {
+                 $push:{products:cartItems},
+                 $inc:{cartTotal:cartItems.quantity, cartPrice: product.onSale ?
+                       product.salePrice
+                      :
+                      product.price },
+                 },
+                 { 
+                 new:true
+                 }).populate('products.product',{price:1,name:1,onSale:1,salePercentage:1,salePrice:1})
+         }else{
+             foundObject.quantity += 1
+            if(cart && foundObject){
+             cart.cartPrice += foundObject.product.onSale ? 
+             foundObject.product.salePrice
+             : 
+             foundObject.product.price
+             cart.cartPrice = evenRound(cart.cartPrice,2) 
+             cart.cartTotal += 1        
+             updatedCart = await cart.save()   
+            }
+         }
+         return updatedCart
+}
+
+
+
+const addToCartGuest = (guestCart:CartTypes,cartItems:CartTypes['products'][0],productId:string,product:ProductDb) => {
+    const foundObject = guestCart?.products?.
+        find(product => productId === product.product.toString())
+
+    if(!guestCart){
+        guestCart = {
+            products:[] as unknown as CartTypes['products'],
+            cartPrice:0,
+            cartTotal:0
+        } 
+        
+    }if(foundObject && guestCart){
+        foundObject.quantity += 1
+        guestCart.cartPrice += product.onSale ?
+        product.salePrice as number
+        :
+        product.price
+        guestCart.cartTotal += 1
+        guestCart.cartPrice = evenRound(guestCart.cartPrice,2)
+    }else{
+        guestCart.products.push(cartItems)
+        guestCart.cartPrice += product.onSale ?
+        product.salePrice as number
+        :
+        product.price
+        guestCart.cartTotal += 1
+    }
+
+    guestCart.cartPrice = evenRound(guestCart.cartPrice,2)
+    return guestCart
+}
+
+
+const deleteFromCartUser = async (user:UserTypes,productId:string,) => {
+    const userShopping = user.shoppingCart
+    const cart = await Cart.findById(userShopping).populate('products.product',
+        {price:1,name:1,onSale:1,salePercentage:1,salePrice:1}) 
+    const foundObject = cart?.products.
+        find(product => productId === product.product.id.toString()) as CartType | undefined
+    
+    if(!foundObject){
+       return ({
+        status:404,
+        response:{Error: 'Product Not Found'}
+       })
+    }
+    if(cart && cart?.products.length < 1){
+        return ({
+            status:400,
+            response:{Error: 'No Items In Cart'}
+           })
+    }else{
+        if(foundObject){
+            const updatedCart = await Cart.findByIdAndUpdate(cart?._id,
+                {
+                $pull:{products:{product:productId}},
+                $inc:{cartTotal: -foundObject?.quantity, cartPrice: evenRound(
+                    foundObject.product.onSale ?
+                     evenRound(-(foundObject.product.salePrice * foundObject.quantity),2)
+                    :
+                    evenRound(-(foundObject.product.price * foundObject.quantity),2),2
+                ) 
+                     }
+                },
+                { 
+                new:true
+                })
+                return ({
+                    status:200,
+                    response:updatedCart
+                   })
+        }
+        return
+    }
+}
+
+const deleteFromCartGuest = async(guestCart:CartTypes,productId:string) => {
+    
+            const foundProduct = guestCart.products.find(product => productId === product.product.toString())
+            const product = await Product.findById(foundProduct?.product)
+            if(guestCart.products.length <= 0){
+                return ({
+                    status:400,
+                    response:{Error: 'No Items In Cart'}
+                })
+            }
+            if(foundProduct && product){
+                const index = guestCart.products.findIndex(product => product.product.toString() !== productId)
+                guestCart.products.splice(index,1)
+                guestCart.cartPrice -= product?.onSale ? 
+                evenRound((product.salePrice as number) * foundProduct.quantity,2) :
+                evenRound(product.price * foundProduct.quantity,2)
+                guestCart.cartPrice = evenRound(guestCart.cartPrice,2)
+                guestCart.cartTotal -= foundProduct.quantity
+                return ({
+                    status:200,
+                    response:guestCart})
+            }else{
+                return ({
+                    status:400,
+                    response:{Error: 'Product Not Found'}
+                   })
+            }
+            
+}
+
+
+const reduceFromCartUser = async (user:UserTypes,productId:string) => {
+
+    const cart = await Cart.findById(user.shoppingCart).populate('products.product',{price:1,name:1,onSale:1,salePercentage:1,salePrice:1}) 
+    const foundObject = cart?.products.find(product => productId === product.product.id.toString()) as CartType | undefined
+    let response 
+    if(foundObject && cart){
+        if(cart.products.length < 1){
+        response =  ({
+                status:400,
+                response:{Error:'Cart Is Empty'}
+            })
+        }
+        if(foundObject.quantity > 1){
+            foundObject.quantity -= 1
+        }else{
+            const index = cart.products.findIndex(product => product.product.toString() !== productId)
+            cart.products.splice(index,1)
+        }
+        cart.cartTotal -= 1
+        cart.cartPrice -= foundObject.product.onSale ? 
+        foundObject.product.salePrice
+        : 
+        foundObject.product.price 
+        cart.cartPrice = evenRound(cart.cartPrice,2)
+        const savedCart = await cart.save()
+        response = ({
+            status:200,
+            response:savedCart
+        })
+    }
+    return (response)
+}
+
+const reduceFromCartGuest = async (guestCart:CartTypes,productId:string) => {
+
+            const foundProduct = guestCart.products.find(product => productId === product.product.toString())
+            const product = await Product.findById(foundProduct?.product) as ProductDb
+            const foundIndex = guestCart.products.findIndex(product => productId === product.product.toString())
+            if(foundProduct && foundProduct.quantity > 1){
+                foundProduct.quantity -= 1
+            }else{
+                guestCart.products.splice(foundIndex,1)
+            }
+            guestCart.cartPrice -= product.onSale ? 
+            product.salePrice as number
+            :
+            product.price
+            guestCart.cartTotal -= 1
+            guestCart.cartPrice = evenRound(guestCart.cartPrice,2)
+            return guestCart
+}
+
+
+export {
+    addToCartUser,
+    addToCartGuest,
+    deleteFromCartUser,
+    deleteFromCartGuest,
+    reduceFromCartGuest,
+    reduceFromCartUser
+
+}
