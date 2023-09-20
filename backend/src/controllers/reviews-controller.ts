d016a5eef82b3665513d9985128f25d9554ed1aa
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-09-20 15:52:52 -0400

    move review db logic to separate file

---
+++ backend/src/controllers/reviews-controller.ts
@@ -0,0 +1,64 @@
+import { NextFunction, Request, Response } from "express";
+import Product from "../models/product-model";
+import { ReviewTypes } from "../types/type";
+import { addNewRating, deleteProductReview, updateProductRating, updatedRating } from "../services/reviews-service";
+
+
+
+
+const postReview = async (req:Request,res:Response,next:NextFunction) => {
+    try{
+        const {user} = req
+        const id = req.params.id
+        const body = req.body as ReviewTypes
+
+        const product = await Product.findById(id)
+
+        if(product){
+            const alreadyRated = product?.reviews?.find(
+                userId => userId.postedBy.toString() === user.id.toString()
+            )
+            if(alreadyRated){
+                await updateProductRating(alreadyRated,body.rating,body.reviewDesc,id)
+            }else{
+                await addNewRating(id,user,body.rating,body.reviewDesc)
+            }
+            const finalProduct = await updatedRating(id)
+    
+            res.json(finalProduct)
+        }else{
+            res.status(404).json({Error:'Item Not Found'})
+        }
+    }catch(error){
+        next()
+    }
+}
+
+const deleteReview = async (req:Request,res:Response,next:NextFunction) => {
+    try{
+        const {user} = req
+
+        const id = req.params.id
+
+        const product = await Product.findById(req.params.id)
+
+        const reviewPosted = product?.reviews?.find(
+            userId => userId.postedBy.toString() === user.id.toString()
+        )
+        if(reviewPosted && (product && product.reviews) && user.reviews){
+
+            await deleteProductReview(product,reviewPosted,user,id)
+
+            const finalProduct = await updatedRating(id)
+            
+            res.json(finalProduct)
+        }
+        else{
+            res.status(404).json({Error:'No Review Or Product Found'})
+        }
+    }catch(error){
+        next()
+    }
+}
+
+export {postReview,deleteReview}
---
+++ backend/src/services/reviews-service.ts
@@ -0,0 +1,86 @@
+import Product from "../models/product-model";
+import User from "../models/user-model";
+import { ProductDb, UserTypes } from "../types/type";
+import evenRound from "../utils/rounding";
+import { Types } from "mongoose";
+
+
+const updatedRating = async (id:string) => {
+    const getAllReviews = await Product.findById(id)
+        const totalReviews = getAllReviews?.reviews?.length
+        let actualRating
+        const reviewSum = getAllReviews?.reviews?.map((product) => product.rating)
+        .reduce((prev,curr) => prev + curr, 0)
+        if(reviewSum && totalReviews){
+             actualRating = evenRound((reviewSum/totalReviews),3)
+             getAllReviews.overallRating = actualRating
+        }
+        const product = await getAllReviews?.save()
+
+        return product
+}
+
+const updateProductRating = async (alreadyRated:ProductDb['reviews'][0],rating:number,reviewDesc:string,id:string) => {
+    await Product.updateOne(
+        {
+        reviews:{
+            $elemMatch:alreadyRated
+        },
+        },{
+        $set:{
+            "reviews.$.rating":rating,
+            "reviews.$.reviewDesc":reviewDesc}
+        },{
+            new:true,
+            context:"query"
+        }
+    )
+    await User.updateOne(
+        {
+            reviews:{
+                $elemMatch:{
+                    product:id
+                }
+            }
+        },{
+            $set:{
+                "reviews.$.rating":rating,
+                "reviews.$.reviewDesc":reviewDesc
+            }
+        }
+    )
+}
+
+const addNewRating = async (id:string,user:UserTypes,rating:number,reviewDesc:string) => {
+    await Product.findByIdAndUpdate(id,{
+        $push: {
+            reviews:{
+                postedBy:user.id,
+                rating:rating,
+                reviewDesc:reviewDesc, 
+            }
+        },
+        new:true,
+        context:'query'
+    })
+    if(user.reviews){
+        user.reviews.push({
+            product:new Types.ObjectId(id),
+            reviewDesc:reviewDesc,
+            rating:rating,})
+        await user.save()
+    }
+}
+
+const deleteProductReview = async (product:ProductDb,reviewPosted:ProductDb['reviews'][0],user:UserTypes,id:string) => {
+    const productIndex = product.reviews.findIndex(review => review.postedBy === reviewPosted.postedBy)
+    product.reviews = product.reviews.splice(productIndex,1)
+    if(user.reviews){
+        const userIndex = user.reviews.findIndex(review => review.product.toString() === id)
+        user.reviews = user.reviews.splice(userIndex,1) 
+    }
+    await user.save()
+    await product.save()
+}
+
+export {updateProductRating,updatedRating,addNewRating,deleteProductReview}
