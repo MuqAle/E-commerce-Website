a707cf52c98f764517cee50566f92f71cc36a3a8
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-09-25 18:36:59 -0400

    create session if no session

--- backend/src/controllers/cart-controller.ts
+++ backend/src/controllers/cart-controller.ts
@@ -8,7 +8,7 @@ import { addToCartGuest,
     reduceFromCartUser} from "../services/cart-service"
 import Cart from "../models/shopping-cart-model"
 import { NextFunction, Request, Response } from "express"
-import {  CartTypes, ProductDb} from "../types/type"
+import {  CartTypes, GuestCartTypes, ProductDb} from "../types/type"
 import Product from "../models/product-model"
 
 
@@ -16,10 +16,33 @@ import Product from "../models/product-model"
 
 
 
-const getAllCart = async(_req:Request,res:Response,) => {
+const getAllCart = async(_req:Request,res:Response) => {
     const cart:CartTypes[] = await Cart.find({}).populate('products.product', {name:1,price:1})
 
      res.json(cart)
+}
+
+const getUserCart = async(req:Request,res:Response) => {
+    try{if(req.user){
+        const cart = await Cart.findById(req.user.shoppingCart).populate('products.product', {})
+        return res.json(cart)
+    }if(req.session.guestCart){
+        const session = req.session.guestCart 
+        return res.json(session)
+    }if(!req.session.guestCart){
+        const session = {
+            products:[] as unknown as GuestCartTypes['products'],
+            cartPrice:0,
+            cartTotal:0
+        } 
+        req.session.guestCart = session
+        return res.json(req.session.guestCart)
+    }
+    else{
+        return res.status(404).json({Error:'No shopping cart found'})
+    }}catch(error){
+        return res.status(500).json({Error:'Internal System Error'})
+    }
 }
 
 const addToCart = async (req:Request,res:Response,next:NextFunction) => {
@@ -33,20 +56,27 @@ const addToCart = async (req:Request,res:Response,next:NextFunction) => {
         const user = req.user
 
         if(product){
-
-            if(user){
-                const updatedCart = addToCartUser(user,productId,cartItems,product)
-    
-                res.status(200).json(updatedCart)
-            }
-            else{
-                const guestCart = req.session.guestCart as CartTypes
-                
-                const newGuestCart = addToCartGuest(guestCart,cartItems,productId,product)
-    
-                req.session.guestCart = newGuestCart
-       
-                res.status(200).json(newGuestCart)
+            if(product.stock === 0){
+                res.status(400).json({Error:'Item Out Of Stock'})
+            }else{
+                if(user){
+                    const updatedCart = await addToCartUser(user,productId,cartItems,product)
+        
+                    res.status(200).json(updatedCart)
+                }
+                else{
+                    const guestItems = {
+                        product:product,
+                        quantity:1
+                    }
+                    const guestCart = req.session.guestCart as GuestCartTypes
+                    
+                    const newGuestCart = addToCartGuest(guestCart,guestItems,productId,product)
+        
+                    req.session.guestCart = newGuestCart
+           
+                    res.status(200).json(newGuestCart)
+                }
             }
         }else{
             res.status(404).json({Error:'Item Not Found'})
@@ -70,7 +100,7 @@ const deleteFromCart = async (req:Request,res:Response,next:NextFunction) => {
                 res.status(updatedCart.status).json(updatedCart.response)
             }
         }else{
-            const guestCart =  req.session.guestCart as CartTypes
+            const guestCart =  req.session.guestCart as GuestCartTypes
             const updatedCart = await deleteFromCartGuest(guestCart,productId)
   
             res.status(updatedCart.status).json(updatedCart.response)
@@ -118,5 +148,6 @@ const reductionCart = async (req:Request,res:Response,next:NextFunction) => {
 export {addToCart,
     deleteFromCart,
     reductionCart,
-    getAllCart}
+    getAllCart,
+    getUserCart}
 
