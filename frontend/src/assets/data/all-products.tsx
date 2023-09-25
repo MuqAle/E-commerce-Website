fae303431c57a1ec9ebae19fe5d0a551cd2ef1a0
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-09-25 14:10:08 -0400

    deleted assets

--- frontend/src/assets/data/all-products.tsx
+++
@@ -1,7 +0,0 @@
-import braceletData from "./bracelet-data";
-import earringData from "./earrings-data";
-import necklaceData from "./necklace-data";
-
-const allData = [...braceletData,...earringData,...necklaceData]
-
-export default allData
