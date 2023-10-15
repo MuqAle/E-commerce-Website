effa76d097078d0a1ae0533609b0909036af630e
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-10-15 17:54:48 -0400

    create star rating component for form

---
+++ frontend/src/pages/reviews-components/star-rating-form.tsx
@@ -0,0 +1,35 @@
+import {FaStar} from 'react-icons/fa'
+import { useImmer } from 'use-immer'
+
+const StarRatingForm = () => {
+
+    const [rating,setRating] = useImmer<null | number>(null)
+    const [hover, setHover] = useImmer<null | number>(null)
+
+    return(
+        <div>
+            {[...Array(5).map((_star,i) => {
+                const ratingValue = i + 1
+                return(
+                    <label>
+                        <input
+                            type='radio'
+                            name='rating-form'
+                            value={ratingValue}
+                            onClick={() => setRating(ratingValue)}
+                        />
+                        <FaStar
+                            className = 'star'
+                            color = {ratingValue <= (hover ?? rating ?? 0) ? "e7b9c0": "e4e5e9"}
+                            onMouseEnter = {() => setHover(ratingValue)}
+                            onMouseLeave = {() => setHover(null)}
+                        />
+                    </label>
+                )
+            })]}
+            <p>The rating is {rating}</p>
+        </div>
+    )
+}
+
+export default StarRatingForm
