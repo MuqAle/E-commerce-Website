3a6f982c18385b7760157869ecd26c43291c0041
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-10-24 17:44:12 -0400

    create hook for outside div click

---
+++ frontend/src/hooks/outside-click.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+
+const useOutsideClick = (callback:() => void) => {
+    const ref = useRef<HTMLDivElement>(null)
+  
+    useEffect(() => {
+    const handleClick = (event:MouseEvent) => {
+        if (ref.current && !(ref.current as HTMLDivElement).contains(event.target as Node)) {
+            callback()
+        }
+    }
+            document.addEventListener('click', handleClick,true)
+    
+            return () => {
+                document.removeEventListener('click', handleClick,true)
+            }
+        }, [callback])
+    
+        return ref;
+    }
+    export default useOutsideClick
+
