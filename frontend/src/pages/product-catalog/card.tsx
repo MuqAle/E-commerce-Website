790cbd075ad57bd8ad61eef5a0ed13a79299f906
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-05 21:25:48 -0500

    add star rating

--- frontend/src/pages/product-catalog/card.tsx
+++ frontend/src/pages/product-catalog/card.tsx
@@ -1,37 +1,30 @@
-
 import '../../style/css/card.css'
+import { CardType} from '../../utils/types'
+import StarRating from '../reviews-components/star-rating'
 
-interface CardProp{
-    cardInfo:{
-        name:string,
-        id:string,
-        type:string,
-        price:number,
-        onSale:boolean,
-        salePrice:number,
-        description:string,
-        Gallery:string[],
-    },  
-}
-
-const Card = ({cardInfo}:CardProp) => {
+const Card = ({cardInfo}:CardType) => {
     return(
         <div className='card'>
             <div className='card-img'>
                 {cardInfo.onSale ? 
                 <span className='on-sale'>On Sale</span>:null}
-                <img className = 'jewelry-img' src={cardInfo.Gallery[0]} alt="jewelry_img" loading='lazy'/>
+                <img className = 'jewelry-img' src={cardInfo.images[0]} alt="jewelry_img" loading='lazy'/>
             </div>
             <div className="label">
                 <p className='card-name'>
                     {cardInfo.name}
                 </p>
                     {cardInfo.onSale ?
-                        <p className='price sale-price'>${cardInfo.salePrice.toFixed(2)} <s>${cardInfo.price}</s> </p>
+                        <p className='price sale-price'>${cardInfo.salePrice?.toFixed(2)} <s>${cardInfo.price}</s> </p>
                         :
                         <p className='price '>${cardInfo.price}</p>
                     }
+                    {cardInfo.overallRating === 0 ? 
+                        null
+                        :
+                        <StarRating rating={cardInfo.overallRating} size={'17'} type='card'/>}
             </div>
+            
         </div>
     )
 }
