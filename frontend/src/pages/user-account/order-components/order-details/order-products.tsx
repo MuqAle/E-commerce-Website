de41e73a31353d870ff3d24f4e473ac88273037a
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-05 21:01:33 -0500

    add class names

--- frontend/src/pages/user-account/order-components/order-details/order-products.tsx
+++ frontend/src/pages/user-account/order-components/order-details/order-products.tsx
@@ -5,14 +5,14 @@ import OrderProductComponent from "./order-product-component";
 const OrderProducts = ({products}:{products:CartTypes['products']}) => {
 
     return(
-        <div>
-            <div>
+        <div className="order-products-container">
+            <div className="order-products-container-header">
                 <p>Item</p>
                 <p>Price</p>
                 <p>Qty</p>
                 <p>Amount</p>
             </div>
-            <div>
+            <div className="order-products-container-list">
             {
                  products.map((product) => (
                     <OrderProductComponent key={product.product._id} product={product}/>
