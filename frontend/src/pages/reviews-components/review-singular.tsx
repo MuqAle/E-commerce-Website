92653034efa46b20b2785913f5648175b35db27c
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-05 21:27:03 -0500

    fix small things

--- frontend/src/pages/reviews-components/review-singular.tsx
+++ frontend/src/pages/reviews-components/review-singular.tsx
@@ -1,3 +1,4 @@
+import { useImmer } from "use-immer"
 import { formatTimeDifference } from "../../utils/sort-reviews"
 import { ReviewType } from "../../utils/types"
 import StarRating from "./star-rating"
@@ -7,6 +8,8 @@ import StarRating from "./star-rating"
 const ReviewSingular = ({review}:{review:ReviewType}) => {
     const date = formatTimeDifference(review.datePosted)
     const name = `${review.postedBy.firstName} ${review.postedBy.lastName[0]}.`
+    const [readMore,setReadMore] = useImmer(false)
+    const reviewLength = review.reviewDesc.length <= 200
     return (
         <div className="review"  >
             <div className="review-post-by-container">
@@ -15,9 +18,11 @@ const ReviewSingular = ({review}:{review:ReviewType}) => {
                 <p className="review-posted-date">{`${date}`}</p>
             </div>
             <div className="review-details">
-                
                 <h3 className="review-title">{review.reviewTitle}</h3>
-                <p className="review-description">{review.reviewDesc}</p>
+                <p className="review-description" style={readMore ? {height:'100%'}:{height:'40px',overflow:'hidden'}}>
+                    {review.reviewDesc}
+                </p>
+                <button className="read-more-btn" style={(readMore || reviewLength) ? {display:'none'}:{display:'flex'}}  onClick={() => setReadMore(true)}>Read More</button>
             </div>
         </div>
     )
