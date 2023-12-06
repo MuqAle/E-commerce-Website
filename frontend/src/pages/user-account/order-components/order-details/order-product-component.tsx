7f9ff3bdc0db743cdca37f15f3f17ad1cb956e3c
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-05 21:01:13 -0500

    add class names

--- frontend/src/pages/user-account/order-components/order-details/order-product-component.tsx
+++ frontend/src/pages/user-account/order-components/order-details/order-product-component.tsx
@@ -7,23 +7,23 @@ const OrderProductComponent = ({product}:{product:CartTypes['products'][0]}) => 
     const base = useResolvedPath('/').pathname
     
     return(
-        <div>
+        <div className="order-product-single">
             <img src={product.product.images[0]}/>
-            <div>
+            <div className="order-product-name"> 
                 <Link to={`${base}shop-all/${product.product._id}`}>
                     <p>{product.product.name}</p>
                 </Link>
                 {
                     product.product.salePercentage ? 
-                    <div>
+                    <div className="order-product-price-container">
                         <p><s>{product.product.price}</s></p>
                         <p>{product.product.salePrice}</p>
                     </div>
                     :
-                    <p>{product.product.price}</p>
+                    <p className="order-product-price">{product.product.price}</p>
                 }
-                <p>{product.quantity}</p>
-                <p>{product.product.price}</p>
+                <p className="order-product-quantity">{product.quantity}</p>
+                <p className="order-product-price">{product.product.price}</p>
             </div>
         </div>
     )
