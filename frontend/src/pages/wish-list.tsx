db10a3dabe30d743639c2d32180aa62af63f8f68
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-06 22:40:59 -0500

    user not logged in wishlist msg

--- frontend/src/pages/wish-list.tsx
+++ frontend/src/pages/wish-list.tsx
@@ -4,26 +4,28 @@ import close from '../assets/imgs/svg-imgs/close_FILL0_wght100_GRAD0_opsz48.svg'
 import { Link } from "react-router-dom";
 import '../style/css/wish-list.css'
 import Breadcrumbs from "../components/breadcrumbs";
-import { ProductDb } from "../utils/types";
+import { LoginTypes, ProductDb } from "../utils/types";
 
 interface WishlistProp{
-    array:ProductDb[]
+    array:ProductDb[],
+    user:LoginTypes | null,
     addToCart: (id:string) => void
     deleteFavorite: (id:string) => void
 }
 
 
-const Wishlist = ({array,addToCart,deleteFavorite}:WishlistProp) => {
+const Wishlist = ({array,addToCart,deleteFavorite,user}:WishlistProp) => {
     return (
        <div className="wish-list-container">
             <Breadcrumbs name={null}/>
             <h1 className="title">Wishlist</h1>
-           {array.length === 0 ?
+           {array.length === 0 && user ?
                 <div className="empty">
                     <p>Your Wishlist Is Empty</p>
                     <Link to={'/shop-all'}><button>Shop All Jewelry</button></Link>
                 </div>
-                    :
+                    : 
+            user ?
                 <div className="wish-list-full">
                     {array.map((a) => (
                         <div className="wish-list-product" key={a._id}>
@@ -34,6 +36,12 @@ const Wishlist = ({array,addToCart,deleteFavorite}:WishlistProp) => {
                             <button className="wishlist-cart-btn" onClick={() => addToCart(a._id)}><img src={shoppingBag} alt="shopping bag" /></button>
                         </div>
                         ))}
+                </div>
+                :
+                <div className="empty">
+                    <p>Sign In To Save Products To Your Wishlist</p>
+                    <Link to={'/login'}><button>Login</button></Link>
+
                 </div>}
        </div>
     )
