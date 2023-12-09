87afa9a068268cda0e4766ddf78beddf8f2e125f
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-09 04:23:53 -0500

    add pagination

--- frontend/src/pages/user-account/order-invoice-components/orders.tsx
+++ frontend/src/pages/user-account/order-invoice-components/orders.tsx
@@ -1,9 +1,30 @@
 import { OrderType} from "../../../utils/types"
 import OrderListComponent from "./order-component"
+import Pagination from "../../../components/pagination"
+import { useMemo} from "react"
+import { useImmer } from "use-immer"
+
+
 
 
 
 const UserOrder = ({orders}:{orders:OrderType[]}) => {
+
+    const [currentPage, setCurrentPage] = useImmer(1)
+    const PageSize = 6
+
+
+    const currentData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return orders.slice(firstPageIndex, lastPageIndex);
+      }, [currentPage, orders])
+
+      const scrollOrderTitle = () => {
+        window.scrollTo(0, 0)
+    }
+
+      
     return(
         <div className="order-main-container">
             <h2 className="order-title">Orders</h2>
@@ -12,11 +33,18 @@ const UserOrder = ({orders}:{orders:OrderType[]}) => {
                     orders.length === 0 ? 
                     <p>No Orders Have Been Made</p> 
                     :
-                    orders.map((order) => (
+                    currentData.map((order) => (
                         <OrderListComponent key={order.id} order={order} ></OrderListComponent>
                     ))
                 }
             </div>
+            <Pagination 
+                slideToView={scrollOrderTitle}
+                onPageChange={page => setCurrentPage(page)}
+                totalCount={orders.length }
+                currentPage={currentPage}
+                pageSize={PageSize} siblingCount={1} />
+
         </div>
     )
 }
--- frontend/src/pages/user-account/review-components/reviews.tsx
+++ frontend/src/pages/user-account/review-components/reviews.tsx
@@ -1,10 +1,27 @@
 import { UserTypes } from "../../../utils/types"
 import UserSingularReview from "./singular-review"
+import Pagination from "../../../components/pagination"
+import { useImmer } from "use-immer"
+import { useMemo } from "react"
 
 
 
 
 const UserReviews = ({reviews,token}:{reviews:UserTypes['reviews'],token:string | null},) => {
+
+    const [currentPage, setCurrentPage] = useImmer(1)
+    const PageSize = 6
+
+
+    const currentData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return reviews?.slice(firstPageIndex, lastPageIndex);
+      }, [currentPage, reviews])
+
+      const scrollOrderTitle = () => {
+        window.scrollTo(0, 0)
+    }
    
 
     return(
@@ -13,10 +30,16 @@ const UserReviews = ({reviews,token}:{reviews:UserTypes['reviews'],token:string 
             {
                 reviews?.length === 0 ? 
                 <p>No Reviews Have Been Posted</p> :
-                reviews?.map((review) => (
+                currentData?.map((review) => (
                     <UserSingularReview key={review.product._id} review={review} token={token}/>
                 ))
             }
+            <Pagination 
+                slideToView={scrollOrderTitle}
+                onPageChange={page => setCurrentPage(page)}
+                totalCount={reviews ? reviews.length : 0 }
+                currentPage={currentPage}
+                pageSize={PageSize} siblingCount={1} />
         </div>
     )
 }
