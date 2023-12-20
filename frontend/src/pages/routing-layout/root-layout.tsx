ec91a505e760272d60dd95cd56715befb1b3518a
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-12-20 05:45:24 -0500

    fix typing

--- frontend/src/pages/routing-layout/root-layout.tsx
+++ frontend/src/pages/routing-layout/root-layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Outlet} from "react-router-dom";
 import {useEffect} from 'react'; 
 import { AnimatePresence, motion } from "framer-motion";
